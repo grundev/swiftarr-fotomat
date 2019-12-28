c62ff8f0cc22c9c5bbfd927dc69a7d6afdf672a2
grundev <grundev@prosedev.com>
2019-12-27 17:40:08 -0800

    add Sources, Tests directories

---
+++ Sources/App/routes.swift
@@ -0,0 +1,10 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+}
