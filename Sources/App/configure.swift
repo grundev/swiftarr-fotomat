18f181df1856a7d494864eda9caa16096ddf9ef3
grundev <grundev@prosedev.com>
2019-12-27 18:23:59 -0800

    default to port 8082, 10MB max body size

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,6 +3,13 @@ import Vapor
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
 
+    // run API on port 8082 by default and set a 10MB hard limit on file size
+    let port = Int(Environment.get("PORT") ?? "8082")!
+    services.register {
+        container -> NIOServerConfig in
+        .default(port: port, maxBodySize: 10_000_000)
+    }
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
